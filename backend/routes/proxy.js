2392ef68d2ebfe41b1989bbf43e4522693f29426
nakum-nirali1812 <nirali.1812nakum@gmail.com>
2026-09-18 14:24:31 +0530

    fix: change SW scope to root / for Android Chrome push notification display

--- backend/routes/proxy.js
+++ backend/routes/proxy.js
@@ -42,7 +42,7 @@ router.get('/sw.js', (req, res) => {
   const swContent = fs.readFileSync(swPath, 'utf8');
 
   res.setHeader('Content-Type', 'application/javascript; charset=utf-8');
-  res.setHeader('Service-Worker-Allowed', '/apps/cartncodform/');
+  res.setHeader('Service-Worker-Allowed', '/');
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
